a11bf70199488e151f25098c391e85585e78f846
MateChaitanya <chaitanyamate02@gmail.com>
2024-04-13 13:30:26 +0530

    first commit

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+
+
+function App() {
+  return (
+    <h1>Learning React | I have learn the basic about app.js ,index .js and json perform some opertaion on given file and run after edit and it gets run succesfully.</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+ 
+
+  return (
+    <h1>Learning react with vite | Chaitu</h1>
+  )
+}
+
+export default App
