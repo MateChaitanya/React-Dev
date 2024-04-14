16be029d25c3c9ced18e2fe1a35d3fd259187c09
MateChaitanya <chaitanyamate02@gmail.com>
2024-04-14 19:56:13 +0530

    first commit

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -5,23 +5,20 @@ import './App.css'
 import Card from './components/Card'
 
 function App() {
-  let myObj = {
-    username:"Cahitanya",
-    age:21
-  }
-  let newArr =[1,2,3]
   const [count, setCount] = useState(0)
+  let myObj = {
+    username: "hitesh",
+    age: 21
+  }
+  let newArr = [1, 2, 3]
 
   return (
     <>
-     <h1 className='w-96 bg-white shadow rounded text-black mb-4'>Tailwind Test</h1>
-     <Card channel ="Funaurreact" someObj={newArr}/>
-     <Card/>
-     
+      <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind test</h1>
+      <Card username="chaiaurcode" btnText="click me" />
+      <Card username="hitesh" />
     </>
   )
-
-  
 }
 
 export default App
